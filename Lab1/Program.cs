c766d5ac20529af90fbf42c0d87fe5bebca89f96
Danny McKinney <xxmysteriomanxx@gmail.com>
2025-03-19 03:01:34 +0000

    Modified Lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string name = "Danny";
+        Console.WriteLine("Hello, " + name + "!");
         Console.WriteLine("Welcome to IT-1050!!");
     }
 }
