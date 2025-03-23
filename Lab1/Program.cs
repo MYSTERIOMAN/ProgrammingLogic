097384c3ad610dd470fb8e0dca25e6d6054be5f3
Danny McKinney <xxmysteriomanxx@gmail.com>
2025-03-23 13:38:23 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,11 +1,33 @@
-﻿namespace Lab1;
+namespace Lab1;
 
-class Program
-{
+using System;
+
+  class Program
+  {
     static void Main(string[] args)
     {
-        string name = "Danny";
-        Console.WriteLine("Hello, " + name + "!");
-        Console.WriteLine("Welcome to IT-1050!!");
+      // Step 1
+      Console.WriteLine("Danny McKinney - Programming Logic IT 1050");
+
+      // Step 2
+      /* My name is Danny McKinney
+      and this is IT-1050 - Lab 1 */
+
+      // Step 3
+      int favnum = 4;
+      Console.WriteLine("My favorite number is " + favnum + ".");
+
+      string favlang = "Python";
+      Console.WriteLine("My favorite coding language is " + favlang + ".");
+
+      double totalprograms = 10;
+      Console.WriteLine("I've written about " + totalprograms + " programs before, very basic ones.");
+
+      bool experience = true;
+      Console.WriteLine(experience);
+
+      // Step 5
+      const string school = "Cuyahoga Community College";
+      Console.WriteLine("I go to " + school + ".");
     }
-}
+  }
