01502f22d4d7ecbc95919bdf58e4f7e04b69db2d
Danny McKinney <xxmysteriomanxx@gmail.com>
2025-04-25 17:50:26 +0000

    done!

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f242766377f8cc1106ab573fb08701a9c8b7719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ec94f9556fe2bbf1b09bdc23dc0c355fee3055")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Problem 1
         string[] names = new string[4];
         names[0] = "Spring";
         names[1] = "Summer";
@@ -15,6 +16,7 @@ class Program
             Console.WriteLine(name);
         }
 
+        //Problem 2
         string[] days = new string[7];
         days[0] = "Monday";
         days[1] = "Tuesday";
@@ -35,7 +37,43 @@ class Program
                 Console.WriteLine($"The day is {days[daynum - 1]}");
             }
             
-        
+
+            //Problem 3
+            string[] books = new string[3];
+            books[0] = "Harry Potter";
+            books[1] = "Hunger Games";
+            books[2] = "The Alchemist";
+
+            string[] authors = new string[3];
+            authors[0] = "J. K. Rowling";
+            authors[1] = "Suzanne Collins";
+            authors[2] = "Paulo Coelho";
+
+            for (int i = 0; i < 3; i++) 
+            {
+                Console.WriteLine($"{books[i]} by {authors[i]}");
+            }
+
+
+            //Problem 4
+            int[] temperatures = {65, 72, 78, 70, 68};
+            Array.Sort(temperatures);
+            Console.WriteLine("Sorted Temps: ");
+            foreach (int i in temperatures)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Highest Temp: {temperatures.Max()}");
+            Console.WriteLine($"Highest Temp: {temperatures.Min()}");
     
+
+            //Problem 5
+            int[] nums = {5, 4, 3, 2, 1};
+            Array.Reverse(nums);
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+
     }
 }
